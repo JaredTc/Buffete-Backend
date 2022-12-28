57ed64fa0675ef9e3df2b09dea7a79463db0852f
JaredTc <jareda_g@outlook.es>
2022-12-27 20:46:26 -0600

    test

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
  const PORT = process.env.PORT || 9090
  const DB_HOST = process.env.DB_HOST || 'localhost'
  const DB_USER = process.env.DB_USER || 'root'
- const DB_PASSWORD = process.env.DB_PASSWORD || ""
+ const DB_PASSWORD = process.env.DB_PASSWORD || ''
  const DB_NAME = process.env.DB_NAME || 'buffette'
 
-//  module.exports = PORT;
+
  module.exports = {
     DB_HOST,
     DB_USER,
@@ -12,7 +12,3 @@
     DB_NAME,
     PORT
  }
-//  module.exportsHost = DB_HOST;
-//  module.exportsUsr = DB_USER;
-//  module.exportsPass = DB_PASSWORD;
-//  module.exportsNam = DB_NAME;
