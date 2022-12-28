dd6507950101550cf123eff4c83b45ab0985d9c5
JaredTc <jareda_g@outlook.es>
2022-12-27 20:49:50 -0600

    h

--- src/app.js
+++ src/app.js
@@ -15,22 +15,22 @@ app.use(bodyParser.json());
 //ver las tablas de las base de datos
 app.get('/', (req, res) => {
 
-    res.send('WELCOME TO SERVER')
-    // connection.query("SHOW TABLES", async (err, rows) => {
-    //     if (err) {
-    //         res.json({ msg: err })
-    //     }
+    // res.send('WELCOME TO SERVER')
+    connection.query("SHOW TABLES", async (err, rows) => {
+        if (err) {
+            res.json({ msg: err })
+        }
 
-    //     if (rows == null) {
-    //         return res.status(403)
-    //     }
-    //     else {
-    //         res.send({
-    //             msg: 'Query Success',
-    //             data: rows
-    //         })
-    //     }
-    // })
+        if (!rows) {
+            return res.status(403)
+        }
+        else {
+            res.send({
+                msg: 'Query Success',
+                data: rows
+            })
+        }
+    })
 
 })
 
